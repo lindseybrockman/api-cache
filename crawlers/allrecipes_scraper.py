d5135369cc0fdef9f956c1c868a69211127e3714
Lindsey Brockman <lbrockman@lakeshoreint.com>
2014-04-22 21:27:12 -0500

    scrape, scraper, scrape

--- crawlers/allrecipes_scraper.py
+++ crawlers/allrecipes_scraper.py
@@ -6,32 +6,39 @@ from bs4 import BeautifulSoup
 
 class Scraper(object):
 
-    def __init__(self, response):
-        self.response = response
+    def __init__(self): 
         self.category_links = set()
         self.recipe_links = set()
         self.visited_links = set()
         self.recipe_data = []
+        self.failed_because_redirect = 0
 
-    def scrape(self, response=None):
-        if not response:
-            response = self.response
-
+    def scrape(self, response):
         soup = BeautifulSoup(response.content)
-
+        print "CURRENTLY AT: {}".format(response.url)
+        print "visted {}".format(len(self.visited_links))
+        print "recipes found {}".format(len(self.recipe_links))
+        print "failed GETS: {}".format(self.failed_because_redirect)
         for link in soup.find_all("a"):
-            if 'recipes' in link.get("href"):
-                print "adding category link {}".format(link.get("href")) 
-                category_links.add(link.get("href"))
-            if 'Recipe' in link.get("href"):
-                print "adding recipe link {}".format(link.get("href"))
-                recipe_links.add(link.get("href")
+            href = link.get("href")
+            if href:
+                if href.startswith("/"):
+                    href = "http://allrecipes.com" + href
+                href = href.split("?")[0]
+                if '/recipes/' in href and href not in self.visited_links:
+                    self.category_links.add(href)
+                if '/Recipe/' in href:
+                    self.recipe_links.add(href)
 
         self.visited_links.add(response.url)
 
         for link in self.category_links:
             if link not in self.visited_links:
-                self.scrape(requests.get(link))
+                res = requests.get(link)
+                if not res.url in self.visited_links:
+                    self.scrape(res)
+                else:
+                    self.failed_because_redirect += 1
 
         # and now we've gone through all the category links
         print "Scraped {} category links".format(len(self.category_links))
@@ -39,7 +46,7 @@ class Scraper(object):
         self.build_recipes(self.recipe_links).format(len(self.recipe_links))
 
     def build_recipes(self, recipe_links):
-        for link in recipe_links
+        for link in recipe_links:
             resp = requests.get(link)
             soup = BeautifulSoup(resp.content)
             name = soup.find("h1", {"id": "itemTitle"})
@@ -56,11 +63,7 @@ class Scraper(object):
             }
             self.recipe_data.append(recipe_data)
 
-#soup.find("div", class_="page_navigation_nav").children
-# return links
-print len(links)
-
 if __name__ == '__main__':
     resp = requests.get('http://allrecipes.com/') 
-    scraper = Scraper(resp)
-    scraper.scrape()
+    scraper = Scraper()
+    scraper.scrape(resp)
