04a39fbf3c0e737cd55c32a073c0a4de4d59c6e2
Lindsey Brockman <lbrockman@lakeshoreint.com>
2014-04-29 07:38:40 -0500

    think the scraper is about done

--- crawlers/allrecipes_scraper.py
+++ crawlers/allrecipes_scraper.py
@@ -1,41 +1,49 @@
+import thread
 import requests
 from bs4 import BeautifulSoup
 
-#json will looks something like:
-#{'name': 'name', 'prep_time': 'prep_time', 'cook_time': 'cook_time', 'ingredients': 'ingredients', 'instructions': 'instructions'}
-
 class Scraper(object):
+    """
+    A Scraper object will take in a response object
+    and start recursively crawling that site for
+    recipe data
+    """
 
-    def __init__(self): 
-        self.category_links = set()
+    def __init__(self):
         self._finished = False
+        self.category_links = set()
         self.recipe_links = set()
         self.dead_ends = set()
         self.recipe_data = []
-        self.failed_because_redirect = 0
 
-    def scrape(self, response):
+    def scrape(self, response, stop_size=20000):
         while not self._finished:
+            # if we've crawled this site before,
+            # go to the next link
             if len(self.category_links) != 0:
                 response = self._get_next_link()
-            if response:
+            # if get_next_link returns a new link and
+            # we haven't grabbed more than 20K recipes,
+            # keep scraping
+            if response and len(self.recipe_links) <= stop_size:
                 soup = BeautifulSoup(response.content)
-                print "CURRENTLY AT: {}".format(response.url)
                 print "visted {}".format(len(self.dead_ends))
                 print "recipes found {}".format(len(self.recipe_links))
-                print "failed GETS: {}".format(self.failed_because_redirect)
                 for link in soup.find_all("a"):
                     href = link.get("href")
                     if href:
                         if href.startswith("/"):
                             href = "http://allrecipes.com" + href
+                        # remove get params, unless we are paginating.
                         if len(href.split("?")) == 1 or "Page" not in href.split("?")[1]:
                             href = href.split("?")[0]
+                        # /recipes/ links are categories or articles
                         if '/recipes/' in href and href not in self.dead_ends:
                             self.category_links.add(href)
+                        # /Recipe/ links are actual recipes
                         if '/Recipe/' in href:
                             self.recipe_links.add(href)
-
+                # don't visit the same thing more than once
                 self.dead_ends.add(response.url)
             else:
                 self._finished = True
@@ -43,20 +51,30 @@ class Scraper(object):
         print "Scraped {} unique recipes".format(len(self.recipe_links))
 
     def _get_next_link(self):
+        """
+        Return the next unvisited link
+        """
         for link in self.category_links:
             if link not in self.dead_ends:
                 try:
                     res = requests.get(link)
                     if res.url not in self.dead_ends and res.status_code not in [404, 500]:
+                        self.dead_ends.add(link) 
                         return res
-                    else:
-                        self.dead_ends.add(link)
-                        self.failed_because_redirect += 1
                 except:
                     self.dead_ends.add(link)
         return None
 
-    def build_recipes(self, recipe_links):
+    def build_recipes(self, chunk_size=2000):
+        recipe_list = list(self.recipe_links)
+        range_size = len(recipe_list)/chunk_size
+        for i in xrange(range_size):
+            thread.start_new_thread(self._build_recipes, (recipe_list[i*chunk_size: (i+1)*chunk_size-1], ))
+
+    def _build_recipes(self, recipe_links):
+        """
+        parse recipe data
+        """
         for link in recipe_links:
             resp = requests.get(link)
             soup = BeautifulSoup(resp.content)
@@ -73,6 +91,7 @@ class Scraper(object):
                 "instructions": instructions.text if instructions else '',
             }
             self.recipe_data.append(recipe_data)
+        print "PARSED: {}".format(len(self.recipe_data))
 
 if __name__ == '__main__':
     resp = requests.get('http://allrecipes.com/') 
